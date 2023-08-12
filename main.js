b374587c6c3d5d4ce2ad5448c168e49696a782c0
Abdul Khaliq <nikotin100k@gmail.com>
2023-08-12 19:55:01 +0500

    Modal +

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-let x = document.querySelector(".x");
+let boxmodal = document.querySelector(".box-modal");
 let modal = document.querySelector(".modal");
 let btn = document.querySelector(".btn");
+let x = document.querySelector(".x");
+
+function remove(){
+    modal.classList.remove('modal-hide', 'modal-add');
+}
 
 btn.addEventListener('click' , function(){
-    modal.classList.toggle('modal-add');
+    if (modal.classList.contains('modal-add')){
+        modal.classList.add('modal-hide');
+    } else {
+        modal.classList.add('modal-add');
+        modal.removeEventListener('animationend' , remove);
+    }
 })
 x.addEventListener('click' , function(){
-    modal.classList.remove('modal-add');
+    if (modal.classList.contains('modal-hide')){
+        modal.classList.add('modal-add');
+        modal.removeEventListener('animationend' , remove);
+    } else {
+        modal.classList.add('modal-hide');
+        modal.addEventListener('animationend' , remove);
+    }
 })
